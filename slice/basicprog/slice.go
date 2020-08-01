94a875fb0ec85bc534f87c292ad0b8c318adfeec
pratikshashete04 <pratikshashete04@gmail.com>
2020-08-01 18:48:12 +0530

    added program of slice

---
+++ slice/basicprog/slice.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Creating an array
+	name := [7]string{"Floratechno", "solutions", "Pvt.", "Ltd.", "Pune,", "Maharashtra,", "India."}
+
+	fmt.Println("Name of the company:", name)
+
+	// Creating a slice
+	myslice := name[0:4]
+
+	// Display the slice
+	fmt.Println("Slice :", myslice)
+
+	// Display the length of the slice
+	fmt.Println("The length of the slice is:", len(myslice))
+
+	// Display the capacity of the slice
+	fmt.Println("The capacity of the slice is:", cap(myslice))
+}
